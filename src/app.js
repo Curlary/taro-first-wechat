d0f127a6e39fecba9b7d6eed1250732a6c6d0052
curlary <curlary@grids.tech>
2018-12-28 17:29:24 +0800

    上传资源文件
    Signed-off-by: curlary <curlary@grids.tech>

--- src/app.js
+++ src/app.js
@@ -13,15 +13,42 @@ class App extends Component {
 
   config = {
     pages: [
-      'pages/index/index'
+      'pages/index/index',
+      'pages/discovery/discovery',
+      'pages/more/more',
+      'pages/answer/answer',
+      'pages/question/question'
     ],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
       navigationBarTitleText: 'WeChat',
       navigationBarTextStyle: 'black'
+    },
+    tabBar: {
+      color: "#626567",
+      selectedColor: "#2A8CE5",
+      backgroundColor: "#FBFBFB",
+      borderStyle: "white",
+      list: [{
+        pagePath: "pages/index/index",
+        text: "首页",
+        iconPath: "./asset/images/index.png",
+        selectedIconPath: "./asset/images/index_focus.png"
+      },{
+        pagePath: "pages/discovery/discovery",
+        text: "发现",
+        iconPath: "./asset/images/discovery.png",
+        selectedIconPath: "./asset/images/discovery_focus.png"
+      },
+        {
+          pagePath: "pages/more/more",
+          text: "我的",
+          iconPath: "./asset/images/burger.png",
+          selectedIconPath: "./asset/images/burger_focus.png"
+        }]
     }
-  }
+  };
 
   componentDidMount () {}
 
@@ -40,4 +67,4 @@ class App extends Component {
   }
 }
 
-Taro.render(<App />, document.getElementById('app'))
+Taro.render(<App />, document.getElementById('app'));
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,22 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View} from '@tarojs/components'
 import './index.less'
+import { AtCountDown  ,AtTabBar,AtNoticebar} from 'taro-ui'
 
 export default class Index extends Component {
+
+  constructor () {
+    super(...arguments) ;
+    this.state = {
+      current: 0
+    }
+  }
+  handleClick (value) {
+    this.setState({
+      current: value
+    })
+  }
+
 
   config = {
     navigationBarTitleText: '首页'
@@ -21,8 +35,20 @@ export default class Index extends Component {
   render () {
     return (
       <View className='index'>
-        <Text>Hello 收拾收拾 world!</Text>
+        <AtNoticebar marquee icon='volume-plus'>
+          新天一鹿业有限责任公司双十一钜惠，先到先得，数量有限，赠完为止！
+        </AtNoticebar>
+       <view display: flex>
+         <text>距离活动开始还有</text>
+         <AtCountDown
+           isCard={2}
+           minutes={51}
+           seconds={49}
+         />
+       </view>
       </View>
+
+
     )
   }
 }
