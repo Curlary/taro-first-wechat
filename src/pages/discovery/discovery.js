0e77d7d13fd4eb82564c7cf97d1dadb66f36383f
curlary <curlary@grids.tech>
2019-01-07 17:28:04 +0800

    添加 日期选择控件

--- src/pages/discovery/discovery.js
+++ src/pages/discovery/discovery.js
@@ -1,7 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
-import './discovery.less'
-import { AtForm, AtInput, AtButton } from 'taro-ui'
-
+import { AtForm, AtInput, AtButton,AtAvatar } from 'taro-ui'
+import { View,Picker } from '@tarojs/components'
 
 export default class Discovery extends Component {
 
@@ -9,37 +8,56 @@ export default class Discovery extends Component {
     super(...arguments);
     this.state = {
       userName:'',
-      passWords:''
+      passWords:'',
+      dateSel: '2018-04-22'
 
     }
   }
-  handleChange (userName) {
+  userNameChange (userName) {
     console.log("userName:"+userName);
     this.setState({
-     userName
+      userName
+    })
+  }
+  passWordChange (passWords) {
+    console.log("passWords:"+passWords);
+    this.setState({
+      passWords
     })
   }
    onSubmit (event) {
     console.log(event);
     console.log("userName:"+this.state.userName);
-    console.log("passWords:"+this.state.passWords)
+    console.log("passWords:"+this.state.passWords);
+    console.log("dateSel:"+this.state.dateSel)
+   }
+   onReset () {
+     this.setState({
+       userName:'',
+       passWords:''
+     })
   }
-   onReset (event) {
-    console.log(event)
-  }
+
+  onDateChange = e => {
+    this.setState({
+      dateSel: e.detail.value
+    })
+  };
+
   render () {
-    return (
-      <AtForm
-        onSubmit={this.onSubmit.bind(this)}
-        onReset={this.onReset.bind(this)}
-      >
+    return <AtForm
+      onSubmit={this.onSubmit.bind(this)}
+      onReset={this.onReset.bind(this)}
+    >
+      <view>
+        <AtAvatar circle image='https://jdc.jd.com/img/200' />
         <AtInput
           name='userName'
           title='用户名'
           type='text'
           placeholder='用户名'
           value={this.state.userName}
-          // onChange={this.handleChange.bind(this)}
+          onChange={this.userNameChange.bind(this)}
         />
         <AtInput
           name='passWords'
@@ -47,11 +65,21 @@ export default class Discovery extends Component {
           type='text'
           placeholder='性别'
           value={this.state.passWords}
-          // onChange={this.handleChange.bind(this)}
+          onChange={this.passWordChange.bind(this)}
         />
+        <View className='page-section'>
+          <View>
+            <Picker mode='date' onChange={this.onDateChange}>
+              <View className='pic41ker'>
+                出生日期：{this.state.dateSel}
+              </View>
+            </Picker>
+          </View>
+        </View>
         <AtButton formType='submit'>提交</AtButton>
         <AtButton formType='reset'>重置</AtButton>
-      </AtForm>
-    )
+      </view>
+
+    </AtForm>
   }
 }
